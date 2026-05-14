fbd2769ac2eae89496390b2e215f1bce67f6db03
uli3120 <45213287+uli31@users.noreply.github.com>
2026-05-13 21:43:33 -0600

    correciones de bugs

--- src/services/admi.service.ts
+++ src/services/admi.service.ts
@@ -10,77 +10,77 @@ export class AdmiService {
 
 
   getCampers(){
-    return this.http.get(' https://api-dev.kincamp.com/camper/')
+    return this.http.get('https://api-dev.kincamp.com/camper/')
   }
   getUsers(){
-    return this.http.get(' https://api-dev.kincamp.com/usuario?is_active=true')
+    return this.http.get('https://api-dev.kincamp.com/usuario?is_active=true')
   }
 
   getComments(){
-    return this.http.get(' https://api-dev.kincamp.com/camper_comment/')
+    return this.http.get('https://api-dev.kincamp.com/camper_comment/')
   }
   postComments(info:any){
-    return this.http.post(' https://api-dev.kincamp.com/camper_comment/',info)
+    return this.http.post('https://api-dev.kincamp.com/camper_comment/',info)
   }
   updateComments(info:any,id){
-    return this.http.patch(' https://api-dev.kincamp.com/camper_comment/'+id,info)
+    return this.http.patch('https://api-dev.kincamp.com/camper_comment/'+id,info)
   }
 
   getTempletMasive(){
-    return this.http.get(' https://api-dev.kincamp.com/mailing/template/massive/')
+    return this.http.get('https://api-dev.kincamp.com/mailing/template/massive/')
   }
   getTempletSystem(){
-    return this.http.get(' https://api-dev.kincamp.com/mailing/template/system/')
+    return this.http.get('https://api-dev.kincamp.com/mailing/template/system/')
   }
   getCorreos(){
-    return this.http.get(' https://api-dev.kincamp.com/mailing/campaign/')
+    return this.http.get('https://api-dev.kincamp.com/mailing/campaign/')
   }
   getCorreosInfo(id){
-    return this.http.get(' https://api-dev.kincamp.com/mailing/campaign_info/'+id)
+    return this.http.get('https://api-dev.kincamp.com/mailing/campaign_info/'+id)
   }
 
   
   createTemplate(data){
-    return this.http.post(' https://api-dev.kincamp.com/email/template/',data)
+    return this.http.post('https://api-dev.kincamp.com/email/template/',data)
 
   }
 
   getPlantilla(idioma= 'es'){
-    return this.http.get(' https://api-dev.kincamp.com/get/mailing/template/'+idioma)
+    return this.http.get('https://api-dev.kincamp.com/get/mailing/template/'+idioma)
 
   }
 
   getPlantillSelect(id:any){
-    return this.http.get(' https://api-dev.kincamp.com/email/system/template/'+id)
+    return this.http.get('https://api-dev.kincamp.com/email/system/template/'+id)
 
   }
   getPlantillSelectMaisva(id:any){
-    return this.http.get(' https://api-dev.kincamp.com/email/massive/template/'+id)
+    return this.http.get('https://api-dev.kincamp.com/email/massive/template/'+id)
 
   }
 
   getcorreoEnviados(id:any){
-    return this.http.get(' https://api-dev.kincamp.com/mailing/campaign/'+id)
+    return this.http.get('https://api-dev.kincamp.com/mailing/campaign/'+id)
 
   }
 
   patchPlantilla(id:any,info){
-    return this.http.patch(' https://api-dev.kincamp.com/email/template/'+id,info)
+    return this.http.patch('https://api-dev.kincamp.com/email/template/'+id,info)
 
   }
 
   delet(id:any){
-    return this.http.delete(' https://api-dev.kincamp.com/delete_email_template/'+id)
+    return this.http.delete('https://api-dev.kincamp.com/delete_email_template/'+id)
 
   }
 
   gaetCampsSchoolProx(id:any){
-    return this.http.get(' https://api-dev.kincamp.com/school/'+id+'/upcoming_camps/')
+    return this.http.get('https://api-dev.kincamp.com/school/'+id+'/upcoming_camps/')
 
   }
 
   gaetCampsSchoolAnteriores(id:any){
-    return this.http.get(' https://api-dev.kincamp.com/school/'+id+'/past_camps/')
+    return this.http.get('https://api-dev.kincamp.com/school/'+id+'/past_camps/')
   }
 
 
